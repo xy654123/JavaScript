a918f845e4c836e04184e0e5f293f1aa3924de7e
xy654123 <96267331+xy654123@users.noreply.github.com>
2022-06-01 23:37:44 +0900

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,33 @@
+var age = 21;
+var firstName = "Bro";
+var listName = "Code";
+var online = true;
+var whatever = null;
+
+age = age + 1;
+
+var fullName = firstName +" "+listName;
+
+console.log("Your age is",age);
+console.log("Hello",fullName);
+console.log("Areyou online?:",online);
+console.log(whatever)
+
+
+let mySweetVariable = 420;
+
+console.log(mySweetVariable)
+
+mySweetVariable = 333;
+
+console.log(mySweetVariable)
+
+//let는 재사용이 가능하지만 선언할때값을 넣지않으면 오류가생긴다 선언을
+//어려번 하지못한다 function이나 {}사에서 따로 선언이가능하다
+
+
+const pi = 3.14159;
+
+console.log(pi)
+
+//const는 값을 바꾸면오류가생김 function이나 {}사에서 재사용가능
