7c7287ade41cf8d864c6535488e3a03fe93ca42b
xy654123 <96267331+xy654123@users.noreply.github.com>
2022-06-02 00:13:24 +0900

    Add files via upload

---
+++ arithmetic expressions.js
@@ -0,0 +1,35 @@
+/*
+ arithmetic expression
+ is some combination of
+ operands (values, variables, etc.)
+ operators ( + - * / %)
+ that can be evaluated to a value
+*/
+var friends = 10;
+
+//friends = friends + 1;
+//friends = friends - 1;
+//friends = friends * 2;
+//friends = friends / 2;
+//friends = friends ** 2;
+//var remainingFriends = friends % 3;
+
+//frineds += 1;
+//friends -= 1;
+//friends *= 2;
+//friends /= 2;
+//friends **= 2;
+//friends %= 3;
+
+/*
+ operator precedence
+ 1. parenthesis ()
+ 2. exponents
+ 3. multiply * & divide /
+ 4. addition + & subtraction -
+*/
+
+//friends = 10 + 2 / 2;
+
+console.log(friends)
+
