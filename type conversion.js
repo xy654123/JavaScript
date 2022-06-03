fa0bffccc23eed57b03eb706645576d1c8e2624d
xy654123 <96267331+xy654123@users.noreply.github.com>
2022-06-03 22:11:58 +0900

    Add files via upload

---
+++ type conversion.js
@@ -0,0 +1,24 @@
+
+/*
+var age = window.prompt("Enter your age");
+
+console.log("Happy Birthday!");
+
+age = Number(age);
+
+age += 1;
+
+console.log(typeof age);
+console.log(age);
+
+*/
+
+var myVar;
+
+//myVar = Number("3.14");
+//myVar = String(3.14);
+myVar = Boolean(1);
+//Boolean (0,null,undefind)=false / 아무값이 들어있으면 true
+
+console.log(typeof myVar);
+console.log(myVar);
---
+++ user input.js
@@ -0,0 +1,11 @@
+
+//var myName = window.prompt("What's your name?");
+
+//console.log("Hello",myName);
+
+document.getElementById("myButton").onclick = function(){
+
+    var myName = document.getElementById("myText").value;
+    console.log("Hello",myName)
+
+}
